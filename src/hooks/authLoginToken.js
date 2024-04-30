075f8bee93c496cdc9c6e490dde6960c0c40b783
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-04-30 18:44:42 +0500

    Track large file with Git LFS

--- src/hooks/authLoginToken.js
+++ src/hooks/authLoginToken.js
@@ -2,6 +2,9 @@ const token = localStorage.getItem("token");
 
 export const authToken = () => {
   if (!token) {
+    console.log("salom");
+    console.log("salom");
+    console.log("salom");
     return false;
   }
   return true;
